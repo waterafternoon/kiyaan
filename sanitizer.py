72dca26f787237615511384c757fcc404332b6fc
Van Van Duong <van@vduong.us>
2016-07-04 16:43:27 -0700

    cleaning up
    simplifying readme, deleting unnecessary files, final commit for a while
    until Elbert Moon wants me to create quiz bowl packets.

--- sanitizer.py
+++
@@ -1,8 +0,0 @@
-#takes a shitload of files and converts them into a readable format, with a little bit of cleaning up to make parsing easier
-#sanitization is probably not the right word.
-
-foldername = "bowl"
-
-dirpath = ".\\"+ foldername + "\\*"
-packlist = glob.glob(dirpath)
-
